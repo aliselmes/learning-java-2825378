70ac7d58b6f26577b821573f6bc522531e7c15ec
Alasdair Selmes <a.selmes@gmail.com>
2024-08-25 18:34:14 -0500

    string manipulation

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World from the IntelliJ IDE!");
+        double studentGPA = 3.45;
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
+
     }
 }
