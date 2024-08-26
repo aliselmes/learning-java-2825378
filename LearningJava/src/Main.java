0d4b3744dea2765f2cf52a710b137436ac48a498
Alasdair Selmes <a.selmes@gmail.com>
2024-08-26 18:20:29 -0500

    Classes

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -4,44 +4,17 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static void announceDeveloperTeaTime() {
-        System.out.println("Waiting for developer tea time...");
-        System.out.println("Type a random word and press Enter to start developer tea time.");
-        Scanner input = new Scanner(System.in);
-        input.next();
-        System.out.println("It's developer tea time!");
-    }
-
-    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
-        double tip = tipRate * listedMealPrice;
-        double tax = taxRate * listedMealPrice;
-        double result = listedMealPrice + tip + tax;
-        return result;
-    }
-
-    public static String moveCapitalLettersToFront(String s) {
-        StringBuilder result = new StringBuilder();
-        int capitalIndex = 0;
-        for(int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (Character.isUpperCase(ch)) {
-                result.insert(capitalIndex ++, ch);
-            } else {
-                result.append(ch);
-            }
-        }
-        return result.toString();
-    }
-
     public static void main(String[] args) {
-        //announceDeveloperTeaTime();
-        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .08);
-        System.out.println(groupTotalMealPrice);
+        Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
-        double individualTotalMealPrice = groupTotalMealPrice / 5;
-        System.out.println(individualTotalMealPrice);
+        //using instance method .findArea()
+        double triangleAArea = triangleA.findArea();
+        System.out.println(triangleAArea);
 
-        String word = moveCapitalLettersToFront("Hello1World");
-        System.out.println(word);
+        double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
+
+        System.out.println(Triangle.numOfSides);
     }
 }
